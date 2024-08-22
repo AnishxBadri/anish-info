5ce2fad562039dd3cca494deb153551828dba4fa
AnishxBadri <anishbadri2621@gmail.com>
2024-08-22 22:28:33 +0400

    First commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import vercel from "@astrojs/vercel/serverless";
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), react(), tailwind()],
+  output: "server",
+  adapter: vercel()
 });
