a70f96f187cb70ce7aacd2368548d2bf90f17ff1
AnishxBadri <anishbadri2621@gmail.com>
2024-08-28 20:38:45 +0400

    Deployement Ready test Commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,14 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), react(), tailwind()],
   output: "server",
-  adapter: vercel()
+  adapter: netlify()
 });
